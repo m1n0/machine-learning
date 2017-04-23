42f313d2c1da833afb117a5c03a86f420798a858
m1n0 <mino.lukac@gmail.com>
2017-04-23 15:54:46 +0200

    ML-AZ-PY Feature scaling

--- Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing.py
+++ Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing.py
@@ -24,6 +24,12 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 y = labelencoder_Y.fit_transform(y)
 
-# Splitting the dataset into the Training set and Test set
+# Splitting the dataset into the Training set and Test set   
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
