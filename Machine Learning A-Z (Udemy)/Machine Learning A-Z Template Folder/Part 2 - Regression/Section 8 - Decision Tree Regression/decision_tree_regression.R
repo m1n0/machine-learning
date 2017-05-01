d4017fa1faec5e79f4768d8850dfe9d4f6575501
m1n0 <mino.lukac@gmail.com>
2017-05-01 21:12:38 +0200

    ML-AZ-R Decision tree regression

--- Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.R
+++ Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.R
@@ -17,7 +17,12 @@ dataset = dataset[2:3]
 # test_set = scale(test_set)
 
 # Fitting the Regression Model to the dataset
-# Create your regressor here
+# install.packages('rpart')
+library(rpart)
+regressor = rpart(formula = Salary ~ .,
+                  data = dataset,
+                  control = rpart.control(minsplit = 1))
+
 
 # Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
@@ -37,7 +42,7 @@ ggplot() +
 # Visualising the Regression Model results (for higher resolution and smoother curve)
 # install.packages('ggplot2')
 library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +
