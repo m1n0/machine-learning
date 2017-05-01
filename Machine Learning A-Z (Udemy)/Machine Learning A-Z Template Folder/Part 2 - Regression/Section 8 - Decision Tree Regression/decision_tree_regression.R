02839b8855be8013101460a272426119bc5c33e2
m1n0 <mino.lukac@gmail.com>
2017-05-01 20:46:25 +0200

    ML-AZ Decision tree regression session files

--- Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.R
+++ Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.R
@@ -4,12 +4,20 @@
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
+# Splitting the dataset into the Training set and Test set
+# # install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
 # Fitting the Regression Model to the dataset
-# install.packages('e1071')
-library(e1071)
-regressor = svm(formula = Salary ~ .,
-                data = dataset,
-                type = 'eps-regression')
+# Create your regressor here
 
 # Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
