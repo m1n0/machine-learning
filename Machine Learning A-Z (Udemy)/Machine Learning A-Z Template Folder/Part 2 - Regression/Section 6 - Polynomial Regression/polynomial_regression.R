afd0f1a48ba9a040133c96e13e09fef468900364
m1n0 <mino.lukac@gmail.com>
2017-05-01 11:52:13 +0200

    ML-AZ-R Polynomial regression

--- Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
+++ Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
@@ -11,6 +11,7 @@ lin_reg = lm(formula = Salary ~ .,
 # Fitting polynomial regression
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~.,
              data = dataset)
 
@@ -26,3 +27,19 @@ ggplot() +
   ggtitle('Truth or bluff (linear regression)') +
   xlab('Level') +
   ylab('Salary')
+
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or bluff (polynomial regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Predict results
+y_pred = predict(lin_reg, newdata = data.frame(Level = 6.5))
+y_pred = predict(poly_reg, newdata = data.frame(Level = 6.5,
+                                                Level2 = 6.5^2,
+                                                Level3 = 6.5^3,
+                                                Level4 = 6.5^4))
