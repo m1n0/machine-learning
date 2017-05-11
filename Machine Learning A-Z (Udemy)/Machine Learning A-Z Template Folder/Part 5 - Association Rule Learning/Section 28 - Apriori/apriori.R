de9c3b3a584f67b7f799a2abe071116d87b08bdd
m1n0 <mino.lukac@gmail.com>
2017-05-11 15:59:05 +0200

    ML-AZ Apriori association session files

---
+++ Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.R
@@ -0,0 +1,6 @@
+# Apriori
+
+# Data Preprocessing
+# install.packages('arules')
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
---
+++ Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
@@ -0,0 +1,9 @@
+# Apriori
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
