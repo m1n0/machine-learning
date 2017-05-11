e16a03f94cf9c9766de5486be7f5592cd7eec8c2
m1n0 <mino.lukac@gmail.com>
2017-05-11 22:25:41 +0200

    ML-AZ-PY Apriori association

--- Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
+++ Machine Learning A-Z (Udemy)/Machine Learning A-Z Template Folder/Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
@@ -7,3 +7,11 @@ import pandas as pd
 
 # Data Preprocessing
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+    
+# Training apriori
+from apyori import apriori
+rules = apriori()
