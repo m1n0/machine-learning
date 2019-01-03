e60ef25bdf4dea8c8541140262550ae95baa7472
m1n0 <mino.lukac@gmail.com>
2019-01-03 20:42:51 +0100

    Coursera ML: NN cost function a bit more comments and different var names

--- Machine Learning (Stanford)/machine-learning-ex4/ex4/nnCostFunction.m
+++ Machine Learning (Stanford)/machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,6 +62,8 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% PART 1:
+
 a1 = [ones(m, 1) X];
 a2 = [ones(m, 1) sigmoid(a1 * Theta1')];
 sig = sigmoid(a2 * Theta2');
@@ -72,16 +74,19 @@ Theta1NoBias = Theta1(:, 2:end);
 Theta2NoBias = Theta2(:, 2:end);
 J = (1 / m) * sum(sum(cost)) + (lambda / (2 * m)) * (sum(sum(Theta1NoBias .^ 2)) + sum(sum(Theta2NoBias .^ 2)));
 
+% PART 2:
 delta1 = zeros(size(Theta1));
 delta2 = zeros(size(Theta2));
 
+% Iterate over the examples
 for t = 1:m,
 	a1t = a1(t,:)';
 	a2t = a2(t,:)';
-	sigt = sig(t,:)';
+
+  sigtheta = sig(t,:)';
 	yVectorT = yVector(t,:)';
 
-	d3t = sigt - yVectorT;
+	d3t = sigtheta - yVectorT;
 
 	z2t = [1; Theta1 * a1t];
 	d2t = Theta2' * d3t .* sigmoidGradient(z2t);
@@ -90,17 +95,16 @@ for t = 1:m,
 	delta2 = delta2 + d3t * a2t';
 end;
 
+% PART 3
 Theta1ZeroBias = [ zeros(size(Theta1, 1), 1) Theta1NoBias ];
 Theta2ZeroBias = [ zeros(size(Theta2, 1), 1) Theta2NoBias ];
-Theta1_grad = (1 / m) * delta1 + (lambda / m) * Theta1ZeroBias;
-Theta2_grad = (1 / m) * delta2 + (lambda / m) * Theta2ZeroBias;
-
-% -------------------------------------------------------------
+Theta1Gradient = (1 / m) * delta1 + (lambda / m) * Theta1ZeroBias;
+Theta2Gradient = (1 / m) * delta2 + (lambda / m) * Theta2ZeroBias;
 
 % =========================================================================
 
 % Unroll gradients
-grad = [Theta1_grad(:) ; Theta2_grad(:)];
+grad = [Theta1Gradient(:) ; Theta2Gradient(:)];
 
 
 end
