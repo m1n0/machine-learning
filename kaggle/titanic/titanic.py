98b1cad1ac14ec69a6a32a0f00ce21a7ef153e80
m1n0 <mino.lukac@gmail.com>
2019-03-13 23:38:56 +0100

    Small optimizations, making sure NaN are not present, fixing family size counting, predictions on test data into separate csv files

--- kaggle/titanic/titanic.py
+++ kaggle/titanic/titanic.py
@@ -19,6 +19,7 @@ from sklearn.ensemble import RandomForestClassifier
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
+# Age categories.
 def process_age(df, cut_points, label_names):
     df["Age"] = df["Age"].fillna(-0.5)
     df["AgeCategory"] = pd.cut(df["Age"], cut_points, labels=label_names)
@@ -31,9 +32,9 @@ label_names = ["Missing", "Infant", "Child", "Teenager", "YoungAdult", "Adult", 
 train = process_age(train, cut_points, label_names)
 test = process_age(test, cut_points, label_names)
 
-# Family size does not seem to have any significant influence on the survival rate(????????????).
-train["FamilySize"] = train["SibSp"] + train["Parch"]
-test["FamilySize"] = test["SibSp"] + test["Parch"]
+# Family size.
+train["FamilySize"] = train["SibSp"] + train["Parch"] + 1
+test["FamilySize"] = test["SibSp"] + test["Parch"] + 1
 
 # Encode categorical values
 def add_encoded_columns(df, column):
@@ -48,25 +49,32 @@ for feature in categorical_features:
     train = add_encoded_columns(train, feature)
     test = add_encoded_columns(test, feature)
 
+# Make sure there are no missing values
+train['Fare'] = train['Fare'].fillna((train['Fare'].mean()))
+test['Fare'] = test['Fare'].fillna((test['Fare'].mean()))
 
 # Prepare train/test set.
 columns = ["Fare", "AgeCategory_Child", "AgeCategory_Teenager", "AgeCategory_YoungAdult", "AgeCategory_Adult", "AgeCategory_Senior", "Sex_female", "Sex_male", "Embarked_C", "Embarked_Q", "Embarked_S", "Pclass_1", "Pclass_2", "Pclass_3", "FamilySize"]
 X_all = train[columns]
 y_all = train["Survived"]
 
-
 X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size = 0.2, random_state = 0)
 
+# Prepare classifiers.
 classifiers = {
-    "Logistic Regression": LogisticRegression(random_state = 0, solver="lbfgs", max_iter = 1000),
+    "Logistic Regression": LogisticRegression(random_state = 0, solver="lbfgs", max_iter = 10000),
     "KNN": KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2),
     "SVM": SVC(kernel = 'linear', random_state = 0),
     "Kernel SVM": SVC(kernel = 'rbf', random_state = 0),
     "Gaussian Naive Bayes": GaussianNB(),
     "Decision Tree": DecisionTreeClassifier(criterion = 'entropy', random_state = 0),
-    "Random Forest": RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+    "Random Forest": RandomForestClassifier(criterion = 'entropy', n_estimators = 100, random_state = 0),
 }
 
+holdout = test
+holdout_predictions = {}
+
+# Fit, predict and output.
 for type, classifier in classifiers.items():
     print(f"\n--- {type} ---")
     scores = cross_val_score(classifier, X_all, y_all, cv = 10)
@@ -75,3 +83,14 @@ for type, classifier in classifiers.items():
     max = np.max(scores)
 
     print(f"\nAccuracy: {accuracy}\nMin: {min}\nMax: {max}\n")
+    print(f"Fitting on all data, predicting test data...\n")
+
+    classifier.fit(X_all, y_all)
+    holdout_predictions[type] = classifier.predict(holdout[columns])
+
+    holdout_ids = holdout["PassengerId"]
+    submission_df = {"PassengerId": holdout_ids,
+                     "Survived": holdout_predictions[type]}
+
+    submission = pd.DataFrame(submission_df)
+    submission.to_csv(f"titanic_{type}.csv", index=False)
